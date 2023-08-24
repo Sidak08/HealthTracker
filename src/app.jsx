8113afdfd6fd8df1c64cddd3f42d5866587a8733
Sidak <sidak@Windowslaptop.local>
2023-08-23 22:26:58 -0400

    turning an svg into inline test works going to make the entire ui inter changble rather than hard code git push orgin main

--- src/app.jsx
+++ src/app.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './files/Login'
 import HomePage from "./files/homepage"
-
+import Svg from "./files/test"
 
 function App() {
     return <BrowserRouter>
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<HomePage />} />
+            <Route path='/test' element={<Svg />} />
         </Routes>
     </BrowserRouter>
 
--- src/files/homepage.jsx
+++ src/files/homepage.jsx
@@ -11,6 +11,7 @@ import fire from "../assets/stats/icons8-fire-50-2.png"
 import weatherAPI from "./secret.jsx"
 import axios from 'axios';
 import sun from "../assets/weatherIcon/Group 1-2.svg"
+import Svg from "./test"
 
 let weatherData = {}
 
@@ -55,6 +56,7 @@ const initialData = {
 function HomePage() {
     return (
         <div id='homePage'>
+            {/* <Svg /> */}
             <Greeting />
             <Graph />
             <Stats protein={14} fat={15} suger={34} calories={1300} sleep={10} water={500} />
@@ -247,3 +249,4 @@ const Greeting = () => {
 
 export default HomePage
 
+//make the entire colour schme editable
